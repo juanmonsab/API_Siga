2d02ed3ab4b6b221acae71fc7044addfe222b672
Juan Montes Sabogal <montessabogaljuan@icloud.com>
2023-05-07 23:59:28 -0500

    Busca estudiantes por facultad, semestre y límite

--- src/main/java/co/edu/unisabana/otrosiga/Controller.java
+++ src/main/java/co/edu/unisabana/otrosiga/Controller.java
@@ -20,4 +20,16 @@ public class Controller {
         return new ResponseEntity<>(estudiante, HttpStatus.CREATED);
     }
 
+    @GetMapping("/")
+    public ResponseEntity<List<Estudiante>> buscarEstudiantes(@RequestParam(value = "facultad") String facultad, @RequestParam(value = "semestre", required = false) Integer semestre, @RequestParam(value = "limite", defaultValue = "0") int limite) {
+        List<Estudiante> estudiantesPorFacultad = estudiantes.stream().filter(e -> e.getFacultad().equals(facultad)).collect(Collectors.toList());
+        if (semestre != null) {
+            estudiantesPorFacultad = estudiantesPorFacultad.stream().filter(e -> e.getSemestre() == semestre).collect(Collectors.toList());
+        }
+        if (limite > 0 && estudiantesPorFacultad.size() > limite) {
+            estudiantesPorFacultad = estudiantesPorFacultad.subList(0, limite);
+        }
+        return new ResponseEntity<>(estudiantesPorFacultad, HttpStatus.OK);
+    }
+
 }
