702f01f6ddb540b6047486830fa07f0e973605ed
Juan Montes Sabogal <montessabogaljuan@icloud.com>
2023-05-07 23:51:32 -0500

    Importa clases y paquetes necesarios para el controlador

--- src/main/java/co/edu/unisabana/otrosiga/Controller.java
+++ src/main/java/co/edu/unisabana/otrosiga/Controller.java
@@ -1,4 +1,12 @@
 package co.edu.unisabana.otrosiga;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Controller {
 }
